8614227ac178fe39a2c9f203f6cde663adb7d31c
AmanKhansili <75494826+AmanKhansili@users.noreply.github.com>
2022-10-07 23:55:24 +0530

    02
    Random Number and Love score game

---
+++ randomNumber.js
@@ -0,0 +1,12 @@
+// var n = Math.random();
+// n = n*6;
+// n = Math.floor(n);
+// console.log(n);
+
+/<--------LOVE SCORE CALCULATOR------>/
+prompt("What is your name: ");
+prompt("What is thrie name: ")
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("Your Love Score is " + loveScore + "%");

b2bbb3a17907c7e472acd56f999376883bd3f24d
AmanKhansili <75494826+AmanKhansili@users.noreply.github.com>
2022-10-08 13:03:21 +0530

    Update randomNumber.js

--- randomNumber.js
+++ randomNumber.js
@@ -5,8 +5,13 @@
 
 /<--------LOVE SCORE CALCULATOR------>/
 prompt("What is your name: ");
-prompt("What is thrie name: ")
+prompt("What is thrie name: ");
 
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
-alert("Your Love Score is " + loveScore + "%");
+if(loveScore > 70){
+    alert("Your Love Score is " + loveScore + "%"+" You both made for each other.");
+}
+else{
+    alert("Your Love Score is " + loveScore + "%")
+}

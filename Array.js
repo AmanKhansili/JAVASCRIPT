628ac3afb3ba4e5156ccb1003f1cd0a450f471b2
AmanKhansili <75494826+AmanKhansili@users.noreply.github.com>
2022-10-09 14:53:14 +0530

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,14 @@
+/*<In JavaScript, array is a single variable that is used to store diffrent elements & list of elements and access them by a single variable.>/
+
+/<The includes() method return true,False if an array contains a spacified value.>*/
+
+var guestList = ["Aman" ,"Abhi","Beeru","Karan","Mayank","Bhatt","Khatti", "Panday"];
+
+var Name = prompt("Enter Your Name");
+
+if(guestList.includes(Name)){
+    alert("Welcome");
+}
+else{
+    alert("Try next time")
+}

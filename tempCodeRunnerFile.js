98415cdaaf7e3e5aa724e11b01705026c63165eb
AmanKhansili <75494826+AmanKhansili@users.noreply.github.com>
2022-10-10 20:21:10 +0530

    05

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,9 +1,4 @@
-var output = []
-var count = 1;
-
-function fizzBuzz() {
-    output.push(count);
-    count++;
-
-    console.log(output);
-}
+while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
